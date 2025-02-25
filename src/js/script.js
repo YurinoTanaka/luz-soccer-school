3c8567040b7b86bf6136a7496fa2a0715b88d076
YurinoTanaka <y.chn616@gmail.com>
2025-02-25 18:24:24 +0900

    2025/2/25

--- src/js/script.js
+++ src/js/script.js
@@ -7,4 +7,23 @@ $(function () {
     autoplaySpeed: 5000,
     arrow: false,
   });
+
+  // 要素を取得
+  const button = document.querySelector('.js-backToTop');
+  // クリックイベント
+  button.addEventListener('click', () => {
+    // ページ上部へスクロール
+    window.scroll({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+  // スクロールイベント
+  window.addEventListener('scroll', () => {
+    if(window.scrollY > 100){
+      button.classList.add('is-active');
+    }else{
+      button.classList.remove('is-active');
+    }
+  });
  });
